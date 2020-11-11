8ae61c1e88f6758231113dc617edc9f1a3361e12
Luca Graves <Lgrave264@west-mec.org>
2020-11-11 16:36:58 -0700

    finished

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,37 @@
 var h11 = document.createElement('H1');
-var h12 = document.createTextNode('hello');
+var h12 = document.createTextNode('hello My Name is Luca Graves');
 h11.appendChild(h12);
 document.body.appendChild(h11);
 
 var p1 = document.createElement('P');
-var p2 = document.createTextNode('paragraph');
+var p2 = document.createTextNode('I am a 17 year old german');
 p1.appendChild(p2);
 document.body.appendChild(p1);
+
+var p3 = document.createElement('P');
+var p4 = document.createTextNode('I live in peoria pheonix');
+p3.appendChild(p4);
+document.body.appendChild(p3);
+
+var p5 = document.createElement('P');
+var p6 = document.createTextNode('My dad is from maine and my mom is from germany');
+p5.appendChild(p6);
+document.body.appendChild(p5);
+
+var p7 = document.createElement('P');
+var p8 = document.createTextNode('I have a 28 year old brother and hes the smartest person i know');
+p7.appendChild(p8);
+document.body.appendChild(p7);
+
+var p9 = document.createElement('P');
+var p10 = document.createTextNode('I am currently taking west mec coding but i will be heading towards the police academy after i graduated.');
+p9.appendChild(p10);
+document.body.appendChild(p9);
 
 var img1 = document.createElement('IMG');
 img1.setAttribute('src','Images/me1.jpg');
 document.body.appendChild(img1);
+
+var img2 = document.createElement('IMG');
+img2.setAttribute('src','Images/photos4.jpg');
+document.body.appendChild(img2);
