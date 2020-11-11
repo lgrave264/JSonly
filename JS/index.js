fa8a1fa206c79999e046a11a958ddc06ba1d33b5
Luca graves <lgrave264@west-mec.org>
2020-11-10 18:17:17 -0700

    base structure

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+var h11 = document.createElement('H1');
+var h12 = document.createTextNode('hello');
+h11.appendChild(h12);
+document.body.appendChild(h11);
+
+var p1 = document.createElement('P');
+var p2 = document.createTextNode('paragraph');
+p1.appendChild(p2);
+document.body.appendChild(p1);
+
+var img1 = document.createElement('IMG');
+img1.setAttribute('src','Images/me1.jpg');
+document.body.appendChild(img1);
